b09eaa3dd2ca0f22101d0b871ae600ddc2b74719
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-04 14:06:54 +0800

    add several variables task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine ("Hello word");
+
+using System.Reflection.Metadata.Ecma335;
+
+int value = 100;
+Console.WriteLine(value);
+
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
