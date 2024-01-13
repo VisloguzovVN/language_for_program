fa3db70460ff832518cf5a525aad08ed6cda1121
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-13 18:34:11 +0800

    add Lesson3:Task1

---
+++ Lesson3/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denominator = c + d;
+    double result = numinator / denominator;
+    return result;
+}
+
+double result = CalculateFormula (1, 2, 3, 4);
+
+Console.WriteLine(result);
