7c17f854c70cfc89bbc1604334d68eb4075f67eb
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-13 19:53:28 +0800

    add Lesson3:Task3

---
+++ Lesson3/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void ZeroEventElements(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            array[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int e in array)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEventElements(array);
+PrintArray(array);
