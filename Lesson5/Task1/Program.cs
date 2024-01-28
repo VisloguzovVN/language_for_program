07ade37156ab6067d0699822734a4356af033ebf
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-28 17:03:46 +0800

    add Lesson5:Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Fact(int n)
+{
+    if(n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.Write(Fact(5));
+
+// Шаг с заходом F11
+// Шаг с обходом F10
+// Продолжить  F5 (до следующей точки)
